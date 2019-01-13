fd93a3ea20a3393917546d9dd8cfb0830a8c5108
Brandon Defrancis <suitedpatriot@gmail.com>
2019-01-13 16:17:27 -0500

    firstCommit

---
+++ src/main/java/io/zipcoder/casino/GoFishPlay.java
@@ -0,0 +1,25 @@
+//package io.zipcoder.casino;
+//
+//import GoFish;
+//import io.zipcoder.casino.game.Player;
+//
+//public class GoFishPlay {
+//
+//
+//public static void main(String[]args){
+//    Player player1 = new Player("Jared", 100);
+//    GoFish goFish = new GoFish(player1);
+//
+//    goFish.play();
+//
+////    System.out.println(player.getBank());
+////
+////    player.setBank(player.getBank()+ 200);
+////
+////    System.out.println("New bank amount: " + player.getBank());
+//
+//}
+//
+//    // Fix book logic
+//    // test quit
+//}
